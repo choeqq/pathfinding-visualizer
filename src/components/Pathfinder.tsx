86e748522d943afc53c5ba5b2e3ecd6512881e3f
choeqq <k.chola@op.pl>
2022-03-09 00:11:02 +0100

    Types change

--- src/components/Pathfinder.tsx
+++ src/components/Pathfinder.tsx
@@ -1,43 +1,49 @@
-import React, {useState} from "react";
-import { NodeUITypes } from "../types/node.type";
-import  Node from './Node';
+import React, { useState } from "react";
+import { NodeTypes } from "../types/node.type";
+import Node from "./Node";
 import { createInitialGrid } from "../helpers/create-grid";
 
-const Pathfinder = () => {
-    const [grid, setGrid] = useState<NodeUITypes[][]>(() => createInitialGrid());
-    const [mouseIsPressed, setMouseIsPressed] = useState<boolean>(false);
+const Pathfinder: React.FC = () => {
+  const [grid, setGrid] = useState<NodeTypes[][]>(() => createInitialGrid());
+  const [mouseIsPressed, setMouseIsPressed] = useState<boolean>(false);
 
-    const handleMouseDown = (row:number, col: number) => {};
-    const handleMouseEnter = (row:number, col: number) => {};
-    const handleMouseUp = () => {};
+  const handleMouseDown = (row: number, col: number) => {};
+  const handleMouseEnter = (row: number, col: number) => {};
+  const handleMouseUp = () => {};
 
-    return (<>
-    <button>Visualize Dijkstra's</button>
-    <div>
-        {grid.map((row,rowIdx) => {
-            return (
-                <div key={rowIdx}>
-                    {row.map((node, nodeIdx) => {
-                        const {row,col,isFinish,isStart,isWall} = node;
-                        return (
-                            <Node 
-                                key={nodeIdx}
-                                row={row}
-                                col={col}
-                                isFinish={isFinish}
-                                isStart={isStart}
-                                isWall={isWall}
-                                onMouseDown={(row,col) => handleMouseDown(row,col)}
-                                onMouseEnter={(row,col) => handleMouseEnter(row,col)}
-                                onMouseUp={() => handleMouseUp()}
-                            />
-                        )
-                    }})
-                </div>
-            )
+  return (
+    <>
+      <button>Visualize Dijkstra's</button>
+      <div>
+        {grid.map((row, rowIdx) => {
+          return (
+            <div key={rowIdx}>
+              {row.map((node, nodeIdx) => {
+                const { row, col, isFinish, isStart, isWall } = node;
+                return (
+                  <Node
+                    key={nodeIdx}
+                    row={row}
+                    col={col}
+                    isFinish={isFinish}
+                    isStart={isStart}
+                    isWall={isWall}
+                    onMouseDown={(row: number, col: number) =>
+                      handleMouseDown(row, col)
+                    }
+                    onMouseEnter={(row: number, col: number) =>
+                      handleMouseEnter(row, col)
+                    }
+                    onMouseUp={() => handleMouseUp()}
+                  />
+                );
+              })}
+            </div>
+          );
         })}
-    </div>
-  </>);
+      </div>
+    </>
+  );
 };
 
 export default Pathfinder;
--- src/types/node.type.ts
+++ src/types/node.type.ts
@@ -1,28 +1,14 @@
 export interface NodeTypes {
   col: number;
   row: number;
-  isStart: boolean;
-  isFinish: boolean;
-  distance: number;
-  isVisited: boolean;
-  isWall: false;
-  previousNode: Node | null;
-}
-
-interface CommonTypes {
-  isStart: boolean;
-  isFinish: boolean;
-  isWall: boolean;
-  onMouseDown: (_1: number, _2: number) => void;
-  onMouseEnter: (_1: number, _2: number) => void;
-  onMouseUp: () => void;
-}
-
-export interface NodeUITypes extends CommonTypes {
-  col: number;
-  row: number;
-}
-
-export interface StyledNodeTypes extends CommonTypes {
-  id: string;
+  isStart?: boolean;
+  isFinish?: boolean;
+  isWall?: boolean;
+  distance?: number;
+  isVisited?: boolean;
+  previousNode?: Node | null;
+  onMouseDown?: (_1: number, _2: number) => void;
+  onMouseEnter?: (_1: number, _2: number) => void;
+  onMouseUp?: () => void;
+  id?: string;
 }
